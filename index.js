40177b45866af6e34dfc463e08736d7826ac6159
jolly <vasanth_r@outlook.com>
2023-03-05 13:41:01 +0530

    Updates JS to display only the link form when create message is submitted. * Renames DOM Link for create message form to messageForm. * Adds DOM Link for link input form. * When event triggered hides message-form and displays link-form. * Changes index.js.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 // D.O.M for form
-const form = document.querySelector('form');
+const messageForm = document.querySelector('#message-form');
+// D.O.M link for link form
+const linkForm = document.querySelector('#link-form');
 // D.O.M link for form>input for create message
 const messageInput = document.querySelector('#message-input');
 // D.O.M link for encrypted message input
 const messageLinkInput = document.querySelector('#link-input');
 // Event Listener on form submit
-form.addEventListener('submit', event => {
+messageForm.addEventListener('submit', event => {
     // Prevents submission of form (default behaviour)
     event.preventDefault();
     /*The btoa() method creates a Base64-encoded ASCII string from a binary string (i.e., a string in which each character in the string is treated as a byte of binary data).*/
     const encryptedMessage = btoa(messageInput.value);
+    // Hide the create message-form
+    messageForm.classList.add('hide');
+    // Display the link display form
+    linkForm.classList.remove('hide');
+    // Setting encrypted
     messageLinkInput.value = `${window.location}#${encryptedMessage}`;
     // Selects the link input automatically
     messageLinkInput.select();
