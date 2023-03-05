11ccc54e6f461d69eef3b85843fc8fb938c01515
jolly <vasanth_r@outlook.com>
2023-03-05 12:04:52 +0530

    Adds D.O.M access for the input. * Changes index.js.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // D.O.M for form
 const form = document.querySelector('form');
-// Prevents submission of form (default behaviour)
+// D.O.M for input
+const input = document.querySelector('input');
+// Event Listener on form submit
 form.addEventListener('submit', event => {
+    // Prevents submission of form (default behaviour)
     event.preventDefault();
+    // Sending input message to console to test
+    console.log(input.value);
 });
