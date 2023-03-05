0f74a35269533ace4fe32cfbdd0803c9017f9b71
jolly <vasanth_r@outlook.com>
2023-03-05 13:25:31 +0530

    Adds link input text selection for user ease of use. * Renames link input dom link to messageLinkInput. * Changes index.js.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const form = document.querySelector('form');
 // D.O.M link for form>input for create message
 const messageInput = document.querySelector('#message-input');
 // D.O.M link for encrypted message input
-const copyMessage = document.querySelector('#link-input');
+const messageLinkInput = document.querySelector('#link-input');
 // Event Listener on form submit
 form.addEventListener('submit', event => {
     // Prevents submission of form (default behaviour)
     event.preventDefault();
     /*The btoa() method creates a Base64-encoded ASCII string from a binary string (i.e., a string in which each character in the string is treated as a byte of binary data).*/
     const encryptedMessage = btoa(messageInput.value);
-    copyMessage.value = `${window.location}#${encryptedMessage}`;
+    messageLinkInput.value = `${window.location}#${encryptedMessage}`;
+    // Selects the link input automatically
+    messageLinkInput.select();
 });
