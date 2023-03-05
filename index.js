3d2080787a4943fe0a98d0e6d52533e98220089c
jolly <vasanth_r@outlook.com>
2023-03-05 12:19:07 +0530

    Captures message and display encrypted message. * Creates D.O.M link for encrypted message input. * Gets and converts message to base64 using Fn atob. * Generates and sets encrypted message to encrypted message input.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // D.O.M for form
 const form = document.querySelector('form');
-// D.O.M for input
-const input = document.querySelector('input');
+// D.O.M link for form>input for create message
+const messageInput = document.querySelector('#message-input');
+// D.O.M link for encrypted message input
+const copyMessage = document.querySelector('#link-input');
 // Event Listener on form submit
 form.addEventListener('submit', event => {
     // Prevents submission of form (default behaviour)
     event.preventDefault();
-    // Sending input message to console to test
-    console.log(input.value);
+    /*The btoa() method creates a Base64-encoded ASCII string from a binary string (i.e., a string in which each character in the string is treated as a byte of binary data).*/
+    const encryptedMessage = btoa(messageInput.value);
+    copyMessage.value = encryptedMessage;
 });
